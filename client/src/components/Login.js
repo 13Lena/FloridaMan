4927e044494a5cc2dd90519867a0ffb0e6091ef7
nellie-geek <janellegieck@msn.com>
2019-05-02 15:40:24 -0600

    Login modal.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,43 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import { Link } from "react-router-dom";
-import { ModalActions, Button, Modal  } from "semantic-ui-react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { ModalActions, Modal } from "semantic-ui-react";
 
-class Modal extends Component {
-    state = { open: false }
+function Login(props) {
+    return (
+        <div>
+            <h2 className="loginTitle">Login</h2>
+            <hr />
+            <Form>
+                <FormGroup>
+                    <Label for="email">Email</Label>
+                    <Input type="text" email="email" id="email" placeholder="email" value={props.email} onChange={props.handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="password">Password</Label>
+                    <Input type="password" name="password" id="password" placeholder="password" value={props.password} onChange={props.handleInputChange} />
+                </FormGroup>
+                <Modal.Actions>
+                    <Button onClick={props.handleLogin} color="primary" block>Login</Button>
+                    <p className="signupLink">
+                        <Router>
+                            <Link to="/signup">Don't have an account?  Sign up here</Link>
+                        </Router>
+                    </p>
+                </Modal.Actions>
+            </Form>
 
-    show = size => () => this.setState({ size, open: true })
-    close = () => this.setState({ open: false })
-
-    render() {
-        const { open, size } = this.state
-
-        Login(props) {
-            return (
-                <div>
-                    <Modal size={mini} open={open} onClose={this.close}>
-                        <h2 className="loginTitle">Login</h2>
-                        <hr />
-                        <Form>
-                            <FormGroup>
-                                <Label for="email">Email</Label>
-                                <Input type="text" email="email" id="email" placeholder="email" value={props.email} onChange={props.handleInputChange} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="password">Password</Label>
-                                <Input type="password" name="password" id="password" placeholder="password" value={props.password} onChange={props.handleInputChange} />
-                            </FormGroup>
-                            <Modal.Actions>
-                                <Button onClick={props.handleLogin} color="primary" block>Login</Button>
-                                <p className="signupLink">
-                                    <Link to="/signup">Don't have an account?  Sign up here</Link>
-                                </p>
-                            </Modal.Actions>
-                        </Form>
-                    </Modal>
-                </div>
-            )
-        }
-    }};
-
+        </div>
+    )
+};
 export default Login;
---
+++ client/src/components/Modal.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Link } from "react-router-dom";
+import { Modal } from "semantic-ui-react";
+import Login from "./Login";
+
+class LoginModal extends Component {
+    state = { open: false }
+    show = () => this.setState({ open: true })
+    close = () => this.setState({ open: false })
+
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <Modal size={'mini'} open={this.props.open} onClose={this.close}>
+                    <Login />
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default LoginModal;
