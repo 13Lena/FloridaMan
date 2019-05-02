c64ca8c4c8621f2d7d64cc7326b8114e5cfc8045
Lena Hamilton <hamiltonlena21@gmail.com>
2019-05-02 13:58:04 -0600

    nav component, basic render

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Nav from "./components/Nav";
+import 'semantic-ui-css/semantic.min.css'
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <h1>Hi</h1>
+    <container>
+      <Nav />
+    </container>
   );
 }
 
