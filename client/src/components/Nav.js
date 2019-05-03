d6a305a06d0325e76a5db9c0648213ff34cc8ba1
Natalied84 <n-dreher@hotmail.com>
2019-05-02 18:43:47 -0600

    database seeds and junk

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import {Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Menu } from 'semantic-ui-react'
 
-function Nav() {
-    return(
-        <nav>
-            <h2>Florida Man</h2>
-            <Link to="/">Main</Link>
-            <Link to="/saved">Saved</Link>
-        </nav>
-    );
+
+export default class Nav extends Component {
+
+    state = {}
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+
+    render() {
+        const { activeItem } = this.state
+        return (
+
+            <Menu>
+            <Menu.Item header><h2>Florida Man</h2></Menu.Item>
+            <Menu.Item
+              name='aboutUs'
+              active={activeItem === 'aboutUs'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item name='jobs' active={activeItem === 'jobs'} onClick={this.handleItemClick} />
+            <Menu.Item
+              name='locations'
+              active={activeItem === 'locations'}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
+
+        )
+    }
 }
 
-export default Nav;
