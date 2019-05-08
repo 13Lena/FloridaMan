c24af69ad7150118d83dd4803a1ffac64ff8443c
Natalied84 <n-dreher@hotmail.com>
2019-05-08 15:07:40 -0600

    mongo nonsense

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../../config/middleware/authMiddleware");
 
 router.post("/login", passport.authenticate("local", {
   failureRedirect: "/api/user/unauthorized",
-  failureFlash : true
+  failureFlash: true
 }), function (req, res, next) {
   console.log("sign in successful")
   res.json({
@@ -17,54 +17,49 @@ router.post("/login", passport.authenticate("local", {
 
 
 
-router.post("/signup", function(req, res, next) {
-      db.User.find({username: req.body.username}, function(err, username) {
-        console.log("email: ",username)
-        console.log("length: ",username.length)
+router.post("/signup", function (req, res, next) {
+  console.log(req.body)
+  db.User.find({ username: req.body.username }, function (err, username) {
+    console.log("email: ", username)
+    if (err) throw err;
+    console.log("making new user")
+    let newUser = new db.User({
+      username: req.body.username,
+      password: req.body.password
+    })
 
-        // if (err) throw err;
-        // if (email) {
-        //   return res.json("email is already in use") 
-        //   console.log("user exists...?")
-        // }
-        // if (!email.length) {
-          console.log("making new user")
-          let newUser = new db.User({
-            username: req.body.username,
-            password: req.body.password,
-            loggedIn: true
-          })
-          newUser.password = newUser.generateHash(req.body.password);
-          newUser.save(function(err) {
-            if (err) throw err;
-            console.log("user saved!");
-            res.redirect(307, "/favorites");
-            
-          });
-        // }
-      })     
-  
+    newUser.password = newUser.generateHash(req.body.password);
+    console.log(newUser)
+    db.User.create({
+      username: req.body.username,
+      password: newUser.password
+    }, function (err) {
+      if (err) throw err;
+      console.log("user saved!");
+      res.redirect(307, "/favorites");
+    });
   })
+})
 
-router.get("/unauthorized", function(req, res, next) {
+router.get("/unauthorized", function (req, res, next) {
   res.json({
     error: req.flash("error"),
     message: "user not authenticated"
   });
 });
 
-router.get("/favorites", authMiddleware.isLoggedIn, function(req, res, next) {
+router.get("/favorites", authMiddleware.isLoggedIn, function (req, res, next) {
   res.json({
     user: req.user,
     loggedIn: true
   });
 });
 
-router.get("/logout", authMiddleware.logoutUser, function(req, res, next) {
+router.get("/logout", authMiddleware.logoutUser, function (req, res, next) {
   res.json("User logged out successfully");
 });
 
-router.get("/admin", authMiddleware.isAdmin, function(req, res, next) {
+router.get("/admin", authMiddleware.isAdmin, function (req, res, next) {
   res.json({
     user: req.user,
     loggedIn: true
